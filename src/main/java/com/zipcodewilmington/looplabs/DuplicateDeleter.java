0cdc5ab64f5f55de3f6911d05e55cde190464c5e
WilliamXu <williamxu@zipcoders-MacBook-Pro-90.local>
2019-02-28 17:07:12 -0500

    completed lab

--- src/main/java/com/zipcodewilmington/looplabs/DuplicateDeleter.java
+++ src/main/java/com/zipcodewilmington/looplabs/DuplicateDeleter.java
@@ -1,15 +1,48 @@
 package com.zipcodewilmington.looplabs;
 
+import java.util.Arrays;
+
 /**
  * Created by leon on 1/25/18.
  */
 public abstract class DuplicateDeleter<T> implements DuplicateDeleterInterface<T> {
-    protected T[] array;
+    protected final T[] array;
 
     public DuplicateDeleter(T[] intArray) {
         this.array = intArray;
+        Arrays.sort(array);
     }
 
-    abstract public T[] removeDuplicates(int maxNumberOfDuplications);
-    abstract public T[] removeDuplicatesExactly(int exactNumberOfDuplications);
+    public T[] removeDuplicates(int maxNumberOfDuplications){
+        T[] result = array.clone();
+        for (int i = maxNumberOfDuplications; i <= array.length; i++) {
+            result = removeDuplicatesExactly(i, result);
+        }
+        return result;
+    }
+    
+    public int countNumberOfOccurrences(T objectToEvaluate){
+        int count = 0;
+        for (T t: array) {
+            if (t.equals(objectToEvaluate)){
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public T[] removeDuplicatesExactly(int exactNumberOfDuplications, T[] arrayToSort){
+        T[] clone = arrayToSort.clone();
+        int resultIndex = 0;
+        for(int i = 0; i < arrayToSort.length; i++){
+            if(countNumberOfOccurrences(arrayToSort[i]) != exactNumberOfDuplications){
+                clone[resultIndex++] = arrayToSort[i];
+            }
+        }
+        return Arrays.copyOf(clone, resultIndex);
+    }
+
+    public T[] removeDuplicatesExactly(int exactNumberOfDuplications) {
+        return removeDuplicatesExactly(exactNumberOfDuplications, this.array);
+    }
 }
--- src/main/java/com/zipcodewilmington/looplabs/IntegerDuplicateDeleter.java
+++ src/main/java/com/zipcodewilmington/looplabs/IntegerDuplicateDeleter.java
@@ -1,8 +1,16 @@
 package com.zipcodewilmington.looplabs;
 
+import java.util.Arrays;
+
 /**
  * Created by leon on 1/29/18.
  * @ATTENTION_TO_STUDENTS You are forbidden from modifying the signature of this class.
  */
 public final class IntegerDuplicateDeleter extends DuplicateDeleter<Integer> {
+
+    public IntegerDuplicateDeleter(Integer[] intArray) {
+        super(intArray);
+    }
+
+
 }
